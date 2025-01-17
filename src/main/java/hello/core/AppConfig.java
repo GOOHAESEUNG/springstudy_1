ecb9d29efb4468a658012e4745f8cbcab20d467d
GOOHAESEUNG <amu6675@naver.com>
2025-01-17 13:46:25 +0900

    appConfig 스프링으로 변환

--- src/main/java/hello/core/AppConfig.java
+++ src/main/java/hello/core/AppConfig.java
@@ -6,25 +6,32 @@ import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.*;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
-public class AppConfig {
+@Configuration
+public class AppConfig { //애플리케이션의 설정 정보, 구성 정보
 
     //이 함수 호출 - 멤버 서비스 구현체 생성자 호출
     //생성자 주입
+    @Bean
     public MemberService memberService(){
         return new MemberServiceImpl(memberRepository());
         //comm + option + m 리펙토링
     }
 
-    private MemberRepository memberRepository() {
+    @Bean
+    public MemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     //생성자 주입
     public OrderService orderService(){
         return new OrderServiceImpl(memberRepository(), discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
 //        return new FixDiscountPolicy();
         return new RateDiscountPolicy();
--- src/main/java/hello/core/member/MemberApp.java
+++ src/main/java/hello/core/member/MemberApp.java
@@ -1,18 +1,15 @@
 package hello.core.member;
 
 import hello.core.AppConfig;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
-    //순수한 자바 코드
     public static void main(String[] args) {
-        AppConfig appConfig = new AppConfig();
 
-        //기존
-        //MemberService memberService = new MemberServiceImpl();
-
-        //AppConfig을 통해 멤버 서비스 구현체 생성자 주입
-        MemberService memberService = appConfig.memberService();
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService = applicationContext.getBean("memberService", MemberService.class);
 
         Member member = new Member(Grade.VIP, "memberA", 1L);
         memberService.join(member);
