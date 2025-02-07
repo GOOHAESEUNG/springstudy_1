3b657f9513d07479431032fcad9338490b233155
GOOHAESEUNG <amu6675@naver.com>
2025-02-07 22:36:28 +0900

    필터 예제

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.FilterType;
 
 @Configuration
 @ComponentScan(
+       // basePackages = "hello.core.member", member 패키지 하단에서만 조회 가능
         excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
         //configuration 어노테이션 붙은거 뺌
 )
