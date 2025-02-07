6f7f587537dc16c01a55fa801398033035e15f96
GOOHAESEUNG <amu6675@naver.com>
2025-02-07 21:29:05 +0900

    컴포넌트 스캔과 의존관계 자동 주입

---
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -0,0 +1,14 @@
+package hello.core;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(
+        excludeFilters = @ComponentScan.Filter(type = FilterType.ANNOTATION, classes = Configuration.class)
+        //configuration 어노테이션 붙은거 뺌
+)
+public class AutoAppConfig {
+
+}
--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,10 +1,15 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class MemberServiceImpl implements MemberService {
 
     //DIP 지킴
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
